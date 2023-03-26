7e95386197592c449068f2619dafba7e06100023
Ragazzi147 <91346278+Ragazzi147@users.noreply.github.com>
2023-03-26 19:34:52 -0300

    first commit

---
+++ library/books/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from uuid import uuid4
+# Create your models here.
+
+class Books(models.Model):
+    id_book = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    release_year = models.IntegerField()
+    state = models.CharField(max_length=50)
+    pages = models.IntegerField()
+    publishing_company = models.CharField(max_length=255)
+    create_at = models.DateField(auto_now_add=True)
+
