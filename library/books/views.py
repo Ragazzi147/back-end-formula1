3e973881f2f5fff995ee1bccc692ddd4edf8309e
Ragazzi147 <lucasax147@gmail.com>
2023-04-17 12:59:33 -0300

    Update, Create e Read

--- library/books/views.py
+++ library/books/views.py
@@ -1,11 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from books.forms import CarrosForm
+from books.models import Carros
 # Create your views here.
 
 def home(request):
-    return render(request, 'index.html')
+    data = {}
+    data['db'] = Carros.objects.all()
+    return render(request, 'index.html', data)
 
 def form(request):
     data = {}
     data['form'] = CarrosForm()
     return render(request, 'form.html', data)
+
+def create(request):
+    form = CarrosForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('home')
+    
+def view(request, pk):
+    data = {}
+    data['db'] = Carros.objects.get(pk=pk)
+    return render(request, 'view.html', data )
+
+def edit(request, pk):
+    data = {}
+    data['db'] = Carros.objects.get(pk=pk)
+    data['form'] = CarrosForm(instance=data['db'])
+    return render(request, 'form.html', data)
+
+def update(request, pk):
+    data = {}
+    data['db'] = Carros.objects.get(pk=pk)
+    form = CarrosForm(request.POST or None, instance=data['db'])
+    if form.is_valid():
+        form.save()
+    return redirect('home')
--- library/library/urls.py
+++ library/library/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 from rest_framework import routers
 
 from books.api import viewsets as booksviewsets
-from books.views import home, form
+from books.views import home, form, create, view, edit, update, delete
 route= routers.DefaultRouter()
 
 route.register(r'books', booksviewsets.BooksViewSet, basename="Books" )   
@@ -28,8 +28,12 @@ route.register(r'books', booksviewsets.BooksViewSet, basename="Books" )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('', include(route.urls)),
-    path('', home),
-    path('form/', form),
-    
+    # path('', include(route.urls)),
+    path('', home, name='home'),
+    path('form/', form, name='form'),
+    path('create/', create, name='create'),
+    path('view/<int:pk>/', view, name='view'),
+    path('edit/<int:pk>/', edit, name='edit'),
+    path('update/<int:pk>/', update, name='update'),
+    path('delete/<int:pk>', delete, name='delete')
 ]
