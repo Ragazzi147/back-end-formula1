6aca56df3338abf8f2f903e17506152c1f54c1a3
Ragazzi147 <lucasax147@gmail.com>
2023-04-24 09:01:46 -0300

    Cadastro c/ ajax

---
+++ library/books/static/cadastro.js
@@ -0,0 +1,30 @@
+(function (win, doc) {
+// Ajax do form
+if(doc.querySelector('#form')){
+    let form = doc.querySelector('#form');
+    
+    function sendForm(event)
+    {
+        event.preventDefault();
+        let data = new FormData(form);
+        let ajax = new XMLHttpRequest();
+        let token = doc.querySelectorAll('input');
+        ajax.open('POST', form.action);
+        ajax.setRequestHeader('X-CSRF-TOKEN', token);
+        ajax.onreadystatechange = function()
+        {
+            if(ajax.status === 200 && ajax.readyState === 4){
+                let result = doc.querySelector('#result');
+                result.innerHTML = 'Operação realizada com sucesso!'
+                result.classList.add('alert');
+                result.classList.add('alert-sucess')
+            }
+        }
+        ajax.send(data);
+        form.reset();
+    }
+    form.addEventListener('submit',sendForm,false);
+
+}
+
+})(window, document);
--- library/books/static/confirmar.js
+++ library/books/static/confirmar.js
@@ -15,4 +15,5 @@
         }
     }
 
+
 })(window, document);
